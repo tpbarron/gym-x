5b8b0dcf1cdb65d16f2a0dfd135954a08a0a26cf
Trevor Barron <barron.trevor@gmail.com>
2017-10-12 11:57:43 -0700

    make walker2d image only for testing

--- gym_x/envs/gym_locomotion_envs_x.py
+++ gym_x/envs/gym_locomotion_envs_x.py
@@ -31,11 +31,11 @@ class Walker2DBulletEnvX(Walker2DBulletEnv):
             print("~INF~", state)
             done = True
 
-        # potential_old = self.potential
-        # self.potential = self.robot.calc_potential()
-        # progress = float(self.potential - potential_old)
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
+        progress = float(self.potential - potential_old)
         # NOTE: progress = 0 in expl case
-        progress = 0.0
+        # progress = 0.0
         # if self.robot.body_xyz[0] > self.max_x_dist:
         #     print (self.robot.body_xyz)
         #     self.max_x_dist = self.robot.body_xyz[0]
@@ -102,18 +102,17 @@ class Walker2DVisionBulletEnvX(Walker2DBulletEnvX):
         Walker2DBulletEnvX.__init__(self)
         self.render_dims = render_dims
         # The observation is a combination of joints and image
-        print (self.observation_space, self.observation_space.low, self.observation_space.high)
-        self.observation_space = spaces.Tuple((spaces.Box(low=0, high=255, shape=(1, *render_dims)),
-                                                self.observation_space))
+        # print (self.observation_space, self.observation_space.low, self.observation_space.high)
+        # self.observation_space = spaces.Tuple((spaces.Box(low=0, high=255, shape=(1, *render_dims)),
+        #                                         self.observation_space))
+        self.observation_space = spaces.Box(low=0, high=255, shape=(1, *render_dims))
 
     def get_render_obs(self):
         """
         Compute first-person view from robot
         """
-        euler = p.getEulerFromQuaternion(self.robot.robot_body.current_orientation())
-        yaw, pitch, roll = euler
         x, y, z = self.robot.body_xyz
-        print (x, y, z)
+        # print (x, y, z)
         cameraEyePosition = list([x, y-0.75, 1.0])
         cameraTargetPosition = [x, y, 1.0]
         cameraUpVector = [0, 0, 1]
@@ -134,20 +133,6 @@ class Walker2DVisionBulletEnvX(Walker2DBulletEnvX):
         rgb=img_arr[2] #color data RGB
         gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
         gray = gray.reshape((1, *self.render_dims))
-        # global i
-        # cv2.imwrite('imgs/test'+str(i)+'.png', gray)
-        # print (rgb.shape) (w, h, 4)
-        # from PIL import Image
-        # I = Image.fromarray(rgb)
-        # I = I.convert("L")
-        # I.save('imgs/test'+str(i)+'.png')
-        # rgb = np.array(I)
-        # print (rgb.shape)
-        # I.show()
-        # input("")
-        # I.close()
-        # dep=img_arr[3] #depth data
-        # i += 1
         return gray
 
     def _step(self, a):
@@ -155,27 +140,40 @@ class Walker2DVisionBulletEnvX(Walker2DBulletEnvX):
         Duplicate of super class so that can modify rewards
         """
         state, rew, done, info = super()._step(a)
-        print (state.shape)
-        print (state)
-        input("")
         render = self.get_render_obs()
-        return (render, state), sum(self.rewards), bool(done), {}
+        return render, sum(self.rewards), bool(done), {}
 
     def build_path(self):
         # print (pybullet_data.getDataPath())
         # p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
         p.setAdditionalSearchPath(os.path.join(os.path.dirname(__file__), "assets/")) #used by loadURDF
         self.cube_id = p.loadURDF("cube_black.urdf", basePosition=[-2, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_black.urdf", basePosition=[-2, 1, 1.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_red.urdf", basePosition=[-1, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_red.urdf", basePosition=[-1, 1, 1.5], physicsClientId=self.physicsClientId)
+
+        self.cube_id = p.loadURDF("cube_lime.urdf", basePosition=[0, 1, 1.5], physicsClientId=self.physicsClientId)
         self.cube_id = p.loadURDF("cube_lime.urdf", basePosition=[0, 1, 0.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_blue.urdf", basePosition=[1, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_blue.urdf", basePosition=[1, 1, 1.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_yellow.urdf", basePosition=[2, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_yellow.urdf", basePosition=[2, 1, 1.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_cyan.urdf", basePosition=[3, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_cyan.urdf", basePosition=[3, 1, 1.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_magenta.urdf", basePosition=[4, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_magenta.urdf", basePosition=[4, 1, 1.5], physicsClientId=self.physicsClientId)
+
         self.cube_id = p.loadURDF("cube_white.urdf", basePosition=[5, 1, 0.5], physicsClientId=self.physicsClientId)
+        self.cube_id = p.loadURDF("cube_white.urdf", basePosition=[5, 1, 1.5], physicsClientId=self.physicsClientId)
 
     def _reset(self):
         obs = super()._reset()
-        self.build_path()
+        # self.build_path()
         render = self.get_render_obs()
-        return (render, obs)
+        return render
+        # return (render, obs)

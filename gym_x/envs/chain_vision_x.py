b2d4264739b370644b9b217248795f92ce8cee82
Trevor Barron <barron.trevor@gmail.com>
2017-10-11 21:10:36 -0700

    cleaning up envs

--- gym_x/envs/chain_vision_x.py
+++ gym_x/envs/chain_vision_x.py
@@ -9,68 +9,16 @@ import gym
 from gym import spaces
 import pybullet as p
 import pybullet_data
-# import pybullet_envs
 from pybullet_envs.gym_locomotion_envs import AntBulletEnv
 
-# print("\n".join(['- ' + spec.id for spec in gym.envs.registry.all() if spec.id.find('Bullet')>=0]))
-
-i = 0
-#TODO: update pybullet
 class ChainEnvX(AntBulletEnv):
 
-    def __init__(self,
-                 render_dims=(84, 84)):
+    def __init__(self):
         super(ChainEnvX, self).__init__()
-        self.render_dims = render_dims
-        # The observation is a combination of joints and image
-        self.observation_space = spaces.Tuple((spaces.Box(low=0, high=255, shape=(1, *render_dims)),
-                                                self.observation_space))
-
-    def get_render_obs(self):
-        """
-        Compute first-person view from robot
-
-        """
-        euler = p.getEulerFromQuaternion(self.robot.robot_body.current_orientation())
-        yaw, pitch, roll = euler
-        cameraEyePosition = list(self.robot.body_xyz)
-        cameraTargetPosition = [np.cos(yaw)*np.cos(pitch), np.sin(yaw)*np.cos(pitch), np.sin(pitch)]
-        cameraTargetPosition = [10*x for x in cameraTargetPosition]
-        # cameraTargetPosition = [10, 0., 0.5]
-        cameraUpVector = [0, 0, 1]
-
-        fov = 120
-        aspect = self.render_dims[0] / self.render_dims[1]
-        nearPlane = 0.26 # this ensures outside body, may see limbs
-        farPlane = 2.0 # TODO: perhaps changing this will make the movement more "surprising?"
-
-        # TODO: fix me to be along moving axis
-        viewMatrix = p.computeViewMatrix(cameraEyePosition, cameraTargetPosition, cameraUpVector, physicsClientId=self.physicsClientId)
-        # viewMatrix = p.computeViewMatrixFromYawPitchRoll(camTargetPos, camDistance, yaw, pitch, roll, upAxisIndex)
-        projectionMatrix = p.computeProjectionMatrixFOV(fov, aspect, nearPlane, farPlane);
-        img_arr = p.getCameraImage(self.render_dims[0], self.render_dims[1], viewMatrix, projectionMatrix, renderer=p.ER_BULLET_HARDWARE_OPENGL, physicsClientId=self.physicsClientId)
-
-        # w=img_arr[0] #width of the image, in pixels
-        # h=img_arr[1] #height of the image, in pixels
-        rgb=img_arr[2] #color data RGB
-        gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-        gray = gray.reshape((1, *self.render_dims))
-        # global i
-        # cv2.imwrite('imgs/test'+str(i)+'.png', gray)
-        # print (rgb.shape) (w, h, 4)
-        # from PIL import Image
-        # I = Image.fromarray(rgb)
-        # I = I.convert("L")
-        # I.save('imgs/test'+str(i)+'.png')
-        # rgb = np.array(I)
-        # print (rgb.shape)
-        # I.show()
-        # input("")
-        # I.close()
-        # dep=img_arr[3] #depth data
-        # i += 1
-
-        return gray
+        l = self.observation_space.low
+        h = self.observation_space.high
+        shp = self.observation_space.shape[0]-8
+        self.observation_space = spaces.box(low=l, high=h, shape=(shp,))
 
     def _step(self, a):
         """
@@ -145,8 +93,78 @@ class ChainEnvX(AntBulletEnv):
         self.HUD(state, a, done)
         self.reward += sum(self.rewards)
 
-        render = self.get_render_obs()
+        state = state[8:]
+        return state, sum(self.rewards), bool(done), {}
 
+    def _reset(self):
+        state = super()._reset()[8:]
+        return state
+
+
+class ChainVisionEnvX(AntBulletEnv):
+
+    def __init__(self,
+                 render_dims=(84, 84)):
+        super(ChainVisionEnvX, self).__init__()
+        self.render_dims = render_dims
+        # The observation is a combination of joints and image
+        self.observation_space = spaces.Tuple((spaces.Box(low=0, high=255, shape=(1, *render_dims)),
+                                                self.observation_space))
+
+    def get_render_obs(self):
+        """
+        Compute first-person view from robot
+        """
+        euler = p.getEulerFromQuaternion(self.robot.robot_body.current_orientation())
+        yaw, pitch, roll = euler
+        cameraEyePosition = list(self.robot.body_xyz)
+        cameraTargetPosition = [np.cos(yaw)*np.cos(pitch), np.sin(yaw)*np.cos(pitch), np.sin(pitch)]
+        cameraTargetPosition = [10*x for x in cameraTargetPosition]
+        # cameraTargetPosition = [10, 0., 0.5]
+        cameraUpVector = [0, 0, 1]
+
+        fov = 120
+        aspect = self.render_dims[0] / self.render_dims[1]
+        nearPlane = 0.26 # this ensures outside body, may see limbs
+        farPlane = 2.0 # TODO: perhaps changing this will make the movement more "surprising?"
+
+        # TODO: fix me to be along moving axis
+        viewMatrix = p.computeViewMatrix(cameraEyePosition, cameraTargetPosition, cameraUpVector, physicsClientId=self.physicsClientId)
+        # viewMatrix = p.computeViewMatrixFromYawPitchRoll(camTargetPos, camDistance, yaw, pitch, roll, upAxisIndex)
+        projectionMatrix = p.computeProjectionMatrixFOV(fov, aspect, nearPlane, farPlane);
+        img_arr = p.getCameraImage(self.render_dims[0], self.render_dims[1], viewMatrix, projectionMatrix, renderer=p.ER_BULLET_HARDWARE_OPENGL, physicsClientId=self.physicsClientId)
+
+        # w=img_arr[0] #width of the image, in pixels
+        # h=img_arr[1] #height of the image, in pixels
+        rgb=img_arr[2] #color data RGB
+        gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+        gray = gray.reshape((1, *self.render_dims))
+        # global i
+        # cv2.imwrite('imgs/test'+str(i)+'.png', gray)
+        # print (rgb.shape) (w, h, 4)
+        # from PIL import Image
+        # I = Image.fromarray(rgb)
+        # I = I.convert("L")
+        # I.save('imgs/test'+str(i)+'.png')
+        # rgb = np.array(I)
+        # print (rgb.shape)
+        # I.show()
+        # input("")
+        # I.close()
+        # dep=img_arr[3] #depth data
+        # i += 1
+
+        return gray
+
+    def _step(self, a):
+        """
+        Mostly directly copied from `WalkerBaseBulletEnv` at
+        https://github.com/bulletphysics/bullet3/blob/master/examples/pybullet/gym/pybullet_envs/gym_locomotion_envs.py
+
+        But wanted to have access to all rewards so could manually determine which ones to use
+        """
+        state, rew, done, info = super()._step(a)
+        render = self.get_render_obs()
         obs = (render, state)
         assert self.observation_space.contains(obs)
         return obs, sum(self.rewards), bool(done), {}
@@ -186,13 +204,11 @@ class ChainEnvX(AntBulletEnv):
         self.cube_id = p.loadURDF("cube_white.urdf", basePosition=[5, 2, 0.5], physicsClientId=self.physicsClientId)
 
     def _reset(self):
-        obs = AntBulletEnv._reset(self)
+        obs = super()._reset()
         self.build_path()
         render = self.get_render_obs()
         return (render, obs)
 
-    def _render(self, **kwargs):
-        AntBulletEnv._render(self, **kwargs)
 
 if __name__ == '__main__':
     env = ChainEnvX()
